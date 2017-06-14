285937ea2a7977c6eaa87eddaa5f23e79e3b41c6
brazzittu <brazzittu@brazzittu-K52JB>
2017-06-14 15:28:22 +0200

    Configurazione Spring

---
+++ BaeJira/src/main/java/it/gruppostudio/baeejira/config/SpringConfig.java
@@ -0,0 +1,28 @@
+package it.gruppostudio.baeejira.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "it.gruppostudio.baeejira")
+public class SpringConfig extends WebMvcConfigurerAdapter{
+	
+	@Bean
+    public ViewResolver viewResolver() {
+
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+
+    }
+	
+}

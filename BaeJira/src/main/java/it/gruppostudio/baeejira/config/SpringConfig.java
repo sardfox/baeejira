d894c763e688adc275362586f62bf05241fa4e5c
Pietro <pietrotanca@gmail.com>
2017-06-15 12:42:41 +0200

    config hibernate

--- BaeJira/src/main/java/it/gruppostudio/baeejira/config/SpringConfig.java
+++ BaeJira/src/main/java/it/gruppostudio/baeejira/config/SpringConfig.java
@@ -1,16 +1,27 @@
 package it.gruppostudio.baeejira.config;
 
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
 @ComponentScan(basePackages = "it.gruppostudio.baeejira")
 public class SpringConfig extends WebMvcConfigurerAdapter{
 	
@@ -24,5 +35,49 @@ public class SpringConfig extends WebMvcConfigurerAdapter{
         return viewResolver;
 
     }
+	
+	@Bean(name = "dataSource")
+    public ComboPooledDataSource dataSource() {
+        // a named datasource is best practice for later jmx monitoring
+        ComboPooledDataSource dataSource = new ComboPooledDataSource("jupiter");
+ 
+        try {
+            dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        } catch (PropertyVetoException pve){
+            System.out.println("Cannot load datasource driver (com.mysql.jdbc.Driver) : " + pve.getMessage());
+            return null;
+        }
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false");
+        dataSource.setUser("root");
+        dataSource.setPassword("pietro");
+        dataSource.setMinPoolSize(5);
+        dataSource.setMaxPoolSize(20);
+        dataSource.setMaxIdleTime(3000);
+ 
+        return dataSource;
+    }
+	
+	@Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("it.gruppostudio.baeejira.entity");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        hibernateProperties.put("hibernate.show_sql", true);
+        sessionFactoryBean.setHibernateProperties(hibernateProperties);
+ 
+        return sessionFactoryBean;
+    }
+	
+	@Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager =
+                new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+	
+	
 	
 }
